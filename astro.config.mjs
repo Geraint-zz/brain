63bb5bb54967e87d9325a81b5f3c3ad5436fa1f7
Geraint Ji <Geraint-zz@users.noreply.github.com>
2026-08-03 15:25:51 +0800

    Set brainhealth123.net as canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,2 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://brain-9do.pages.dev', output: 'static' });
+export default defineConfig({ site: 'https://brainhealth123.net', output: 'static' });
